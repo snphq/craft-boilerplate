be8251ddcfe9dc3531dfbff5b02dd14d21baaf0d
Sergey <isakovso@gmail.com>
2018-01-23 19:43:13 +0300

    Add headerContent container

---
+++ src/javascripts/containers/headerContent/headerContent.js
@@ -0,0 +1,2 @@
+import '../../components/menu';
+import './headerContent.scss';
--- src/javascripts/global.js
+++ src/javascripts/global.js
@@ -1,3 +1,4 @@
+import './containers/headerContent';
 import '../stylesheets/styles.scss';
 
 console.log('Global.js file has imported');
